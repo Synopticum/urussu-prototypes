cee3db90287446c116d17b59b7c12b75fdd0d8a1
snovikov <synopticum@gmail.com>
2015-08-12 17:43:08 +0300

    added 'Login Screen' prototypes

---
+++ js/call.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+//    global
+    $('.error, .notice').find('.close').click(function () {
+        $(this).parent().hide();
+    });
+
+//    login
+    $('.vk').click(function () {
+        $('.login-error').fadeIn();
+    });
+
+    $('.anon').click(function () {
+        $('.login-error').hide();
+    });
+
+    $('.support').find('a').click(function (e) {
+        e.preventDefault();
+        $('.login-popup').hide();
+        $('.more').hide();
+        $('.feedback-popup').show();
+        $('.back').show();
+    });
+
+    $('.send').click(function () {
+        $('.send-notice').fadeIn();
+
+        setTimeout(function () {
+            $('.send-notice').fadeOut();
+        }, 3000);
+    });
+
+    $('.back').click(function () {
+        $('.feedback-popup').hide();
+        $('.info-popup').hide();
+        $('.back').hide();
+        $('.login-popup').show();
+        $('.more').show();
+    });
+
+    $('.more').click(function () {
+        $('.login-popup').hide();
+        $('.more').hide();
+        $('.info-popup').show();
+        $('.back').show();
+    });
+});
